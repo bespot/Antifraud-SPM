bcd31550b64d0e6bac9b8e88104c3828cf4b6e15
spvagelis <spirou.vagelis@gmail.com>
2025-01-24 12:37:32 +0200

    - Created release version 1.0.0 for AntifraudSDK - Added in Package.swift file the AntifraudSDK.xcframework and the dependency sharedSPM

--- Package.swift
+++ Package.swift
@@ -5,19 +5,33 @@ import PackageDescription
 
 let package = Package(
     name: "Antifraud-SPM",
+    platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Antifraud-SPM",
+            type: .dynamic,
             targets: ["Antifraud-SPM"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/bespot/sharedSPM.git", exact: "1.0.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Antifraud-SPM"),
+            name: "Antifraud-SPM",
+            dependencies: [
+                .target(name: "AntifraudSDK"),
+                .product(name: "sharedSPM", package: "sharedSPM")
+            ]
+        ),
         .testTarget(
             name: "Antifraud-SPMTests",
             dependencies: ["Antifraud-SPM"]),
+        .binaryTarget(
+            name: "AntifraudSDK",
+            url: "https://github.com/bespot/Antifraud-SPM/releases/download/1.0.0/AntifraudSDK.xcframework.zip",
+            checksum: "c1defeef4ad3ef837602e8732d88e9a3c0960ff4d8f3f5eec6bc2ed01ef9706f")
     ]
 )
